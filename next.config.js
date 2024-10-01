c9ef7b36cf987e60af39cce00effe7ce273e5199
Kristian Reformis <60687151+tianreformis@users.noreply.github.com>
2024-10-01 09:15:06 +0800

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.html$/,
+      type: 'asset/resource', // This tells Webpack to treat HTML files as resources
+    });
 
-module.exports = nextConfig
+    // Add fallbacks for server-side modules
+    config.resolve.fallback = {
+      fs: false,
+      child_process: false,
+      net: false,
+      tls: false,
+    };
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Poppins } from "next/font/google"
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["300", "400"]
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={poppins.className}>
+      <Component {...pageProps} />
+    </div>
+  )
 }
